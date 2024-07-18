6a36a1c10e39d444ee65cbca0b8e8f6c3318c48f
Annabelle <annabelle.bosse.pro@gmail.com>
2024-07-18 11:04:04 +0200

    correction faille

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,8 +13,26 @@ import Home from './components/Home';
 import TrelloBoard from './components/Board';
 import { CssBaseline, Container } from '@mui/material';
 import Profile from './components/Auth/Profile';
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from './components/Firebase'; // Assuming your Firebase configuration import
 
 function App() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
+      setUser(userAuth);
+      setInitializing(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (initializing) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <Router>
       <CssBaseline />
@@ -22,13 +40,13 @@ function App() {
         <Navbar />
         <Container maxWidth="lg" style={{ paddingTop: '2rem' }}>
           <Routes>
-           <Route path="/" element={<Home />} />
-           <Route path="/profile" element={<Profile />} />
-           <Route path="/trello-board/:boardId" element={<TrelloBoard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={user ? <Profile /> : <Navigate to="/login" />} />
+            <Route path="/trello-board/:boardId" element={user ? <TrelloBoard /> : <Navigate to="/login" />} />
+            <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+            <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
             <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/new-board" element={<NewBoard />} />
+            <Route path="/new-board" element={user ? <NewBoard /> : <Navigate to="/login" />} />
           </Routes>
         </Container>
         <Footer />
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { Drawer, List, ListItem, ListItemText, Divider, Button } from '@mui/material';
 import './Sidebar.css';
 import { useNavigate } from 'react-router-dom';
+import { auth } from './Firebase'; // Adjust the import path as per your project structure
 
 const Sidebar = ({ open, onClose }) => {
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    // Add your logout logic here
-    navigate('/login');
+  const handleLogout = async () => {
+    try {
+      await auth.signOut(); // Firebase sign out method
+      navigate('/login'); // Navigate to login page after successful logout
+    } catch (error) {
+      console.error('Error logging out:', error);
+      // Handle any logout errors here
+    }
   };
 
   const handleUserProfile = () => {
